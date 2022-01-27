f02aaf699d0fdca5883b3a420a1221126dd66fae
Timo Sutterer <hi@timo-sutterer.de>
2022-01-27 11:32:33 +0100

    feat: get token balance

--- lambda-fns/auth/VerifyAuthChallengeResponse/index.ts
+++ lambda-fns/auth/VerifyAuthChallengeResponse/index.ts
@@ -1,14 +1,56 @@
+import Web3 from 'web3';
 import { CognitoUserPoolTriggerHandler } from 'aws-lambda';
 
 export const handler: CognitoUserPoolTriggerHandler = async event => {
+    // TODO: use env variables to set rpcUrl and contractAdress
+    const rpcUrl = 'https://rinkeby-light.eth.linkpool.io/';
+    const contractAddress = '';
+    // We only need the balanceOf function
+    const contractABI = [
+        {
+            "constant": true,
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "owner",
+                    "type": "address"
+                }
+            ],
+            "name": "balanceOf",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ];
+
+
     const username = event.request.privateChallengeParameters!.username;
     const loginCode = event.request.privateChallengeParameters!.loginCode;
 
-    // TODO: recover pubKey used to sign challengeAnswer
-    // const signKey = recover(loginCode, event.request.challengeAnswer)
-    //if (signKey === username) {
-    if (username === username) {
-        // TODO: check if user owns NFT/POAP
+    // Recover pubKey that was used to sign challengeAnswer
+    const web3 = new Web3(rpcUrl);
+    const signKey = await web3.eth.personal.ecRecover(loginCode, event.request.challengeAnswer);
+
+    let tokenBalance = 0.0;
+    if (signKey.toLowerCase() === username.toLowerCase()) {
+        // Get token balance
+        const contract = new web3.eth.Contract(contractABI, contractAddress);
+        try {
+            tokenBalance = await contract.methods.balanceOf(signKey).call();
+        } catch (error) {
+            console.log('Failed to retrieve token balance', error);
+        }
+    }
+
+    // Check if user has sufficient tokens
+    if (tokenBalance >= 1.0) {
         event.response.answerCorrect = true;
     } else {
         event.response.answerCorrect = false;
